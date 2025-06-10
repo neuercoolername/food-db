a01cdf7ee34256894a2a2905def247a5c0012908
David Amberg <david.amberg@ymail.com>
2025-06-10 13:27:51 +0200

    point db to neon postgres

--- lib/db.ts
+++ lib/db.ts
@@ -1,18 +1,9 @@
-// import { PrismaClient } from '../app/generated/prisma'
-
-// const globalForPrisma = globalThis as unknown as {
-//   prisma: PrismaClient | undefined
-// }
-
-// export const prisma = globalForPrisma.prisma ?? new PrismaClient()
-
-// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
 import { PrismaClient } from '@prisma/client'
-import { PrismaLibSQL } from '@prisma/adapter-libsql'
 
-const adapter = new PrismaLibSQL({
-  url: `${process.env.TURSO_DATABASE_URL}`,
-  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
-})
-export const prisma = new PrismaClient({ adapter })
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(md|txt|LICENSE)$/,
+      type: 'asset/source',
+    })
+
+    // Ignore these problematic files entirely
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@libsql/isomorphic-ws/README.md': false,
+      '@libsql/client/README.md': false,
+    }
+
+    return config
+  },
 
 };
 
